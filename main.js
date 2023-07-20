dd3d0b2a4aaf4fefa543ee35ad3687ccc3f3616a
rcncln <53015856+rcncln@users.noreply.github.com>
2023-07-20 22:14:53 +0200

    deleted thisAmount as function parameter

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function statement(invoice, plays) {
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
         let thisAmount = 0;
-        thisAmount = amount(play, thisAmount, perf);
+        thisAmount = amount(play, perf);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
@@ -30,25 +30,26 @@ function statement(invoice, plays) {
 }
 
 
-function amount(play, thisAmount, perf) {
+function amount(play, perf) {
+    let result = 0;
     switch (play.type) {
         case "tragedy":
-            thisAmount = 40000;
+            result = 40000;
             if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
+                result += 1000 * (perf.audience - 30);
             }
             break;
             case "comedy":
-                thisAmount = 30000;
+                result = 30000;
                 if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                    result += 10000 + 500 * (perf.audience - 20);
                 }
-                thisAmount += 300 * perf.audience;
+                result += 300 * perf.audience;
                 break;
                 default:
                     throw new Error(`unknown type: ${play.type}`);
     }
-    return thisAmount;
+    return result;
 }
 
 
