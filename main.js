3e377efad32765e4124408ea88246fb9afd49116
rcncln <53015856+rcncln@users.noreply.github.com>
2023-07-20 22:40:00 +0200

    renamed format function to usd

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function statement(invoice, plays) {
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
-    const format = new Intl.NumberFormat("en-US",
+    const usd = new Intl.NumberFormat("en-US",
         {
             style: "currency", currency: "USD",
             minimumFractionDigits: 2
@@ -21,10 +21,10 @@ function statement(invoice, plays) {
         // add extra credit for every ten comedy attendees
         if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+        result += ` ${play.name}: ${usd(thisAmount / 100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
