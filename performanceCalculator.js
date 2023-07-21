41fe23a46360238eed0e066224754fbc2c6ccc91
rcncln <53015856+rcncln@users.noreply.github.com>
2023-07-21 14:02:10 +0200

    added performanceCalculator.js

---
+++ performanceCalculator.js
@@ -0,0 +1,13 @@
+class PerformanceCalculator {
+    constructor(performance, play) {
+        this.performance = performance
+        this.play = play
+    }
+}
+
+
+class tragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        
+    }
+}
